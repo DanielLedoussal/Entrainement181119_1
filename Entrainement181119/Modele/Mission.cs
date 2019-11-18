b484ce16ccae51f5a8e3a08308f819caacb2e5a5
DanielLedoussal <ledoussaldaniel@gmail.com>
2019-11-18 15:01:57 +0100

    Avancement sur la classe mission

--- Entrainement181119/Modele/Mission.cs
+++ Entrainement181119/Modele/Mission.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,47 @@ namespace Entrainement181119
 {
     class Mission
     {
+
+        #region Atribut
+
+        private string _nom;
+        private string _description;
+        private int _nbHeurePrevus;
+        private Dictionary<string, int> _releveHoraire = new Dictionary<string, int>();
+        static ArrayList CollClasseMission = new ArrayList();
+
+        #endregion
+        #region Constructeur
+
+
+        public Mission(string nom, string description, int nbHeurePrevus, Dictionary<string, int> releveHoraire)
+        {
+            Nom = nom;
+            Description = description;
+            NbHeurePrevus = nbHeurePrevus;
+            ReleveHoraire = releveHoraire;
+            Mission.CollClasseMission.Add(this);
+
+
+        }
+        #endregion
+        #region Getter setter
+
+       
+
+        public string Nom { get => _nom; set => _nom = value; }
+        public string Description { get => _description; set => _description = value; }
+        public int NbHeurePrevus { get => _nbHeurePrevus; set => _nbHeurePrevus = value; }
+        public Dictionary<string, int> ReleveHoraire { get => _releveHoraire; set => _releveHoraire = value; }
+        #endregion
+
+        #region Methode
+
+        public Dictionary<string, int> getRevelveHoraire()
+        {
+            
+        }
+
+        #endregion
     }
 }
